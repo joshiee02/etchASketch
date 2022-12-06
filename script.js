c75000babf04b98ffb1f6dcd03deda6dfeb93815
jk <jkhirakosocmed@gmail.com>
2022-12-06 19:45:05 +0900

    add func for creating grid

---
+++ script.js
@@ -0,0 +1,13 @@
+const container = document.querySelector('#container');
+
+function createGrid(){
+    for(let i = 0; i < 16; i++){
+
+        startingDiv = document.createElement('div');
+        container.appendChild(startingDiv);
+        
+    }
+
+}
+
+createGrid();
