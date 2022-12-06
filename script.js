48e2ddbeb15e314c28b4a43c429f0396d4ccbe00
jk <jkhirakosocmed@gmail.com>
2022-12-06 20:45:53 +0900

    change var name for readability

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const container = document.querySelector('#container');
 function createGrid(){
     for(let i = 0; i < 16; i++){
 
-        startingDiv = document.createElement('div');
-        container.appendChild(startingDiv);
-        startingDiv.classList.add('grids');
+        grids = document.createElement('div');
+        container.appendChild(grids);
+        grids.classList.add('grids');
 
     }
 
