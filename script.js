02a015df6a4c3db2190e9c78d9ff4b343b235e48
jk <jkhirakosocmed@gmail.com>
2022-12-07 20:39:15 +0900

    add test code for adding color on grid

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const container = document.querySelector('#container');
 
+let grids = [];
+
 function createGrid(){
+
     for(let i = 0; i < 16; i++){
 
-        grids = document.createElement('div');
-        container.appendChild(grids);
-        grids.classList.add('grids');
-
+        let grid = document.createElement('div');
+        container.appendChild(grid);
+        grid.classList.add('grids');
+        grids.push(grid);
     }
-
 }
-
 createGrid();
+
+for( let grid of grids){
+
+    grid.addEventListener('mouseover', () => {
+        grid.classList.add('color');
+    });
+}
